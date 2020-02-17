6dcef0aa0ab7fb91f29c0fd068e3fdfc2a2f6f34
nikarichardson <nikarichardson@yahoo.com>
2020-02-16 20:35:37 -0800

    add all filler endpoints

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 import subprocess
-from flask import Flask
+from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
+import json
 from models import setup_db, Campsite
 
 def create_app(test_config=None):
@@ -10,8 +11,9 @@ def create_app(test_config=None):
     setup_db(app)
     CORS(app)
 
-    @app.route('/')
-    def get_greeting():
+    @app.route('/campsites', methods=['GET'])
+    def get_campsites():
+        """
         excited = os.environ['EXCITED']
         greeting = "Hello" 
         #person = Person(name="Maria", catchphrase="hello!")
@@ -19,11 +21,96 @@ def create_app(test_config=None):
         if excited == 'true': 
             greeting = greeting + "!!!!!"
             return greeting #+ person.name 
+        """
+        return "Not implemented!"
+
+    @app.route('/campsites/<int:campsite_id>', methods=['GET'])
+    def get_campsites_by_id(campsite_id):
+        return "Not implemented!"
 
 
-    @app.route('/coolkids')
-    def be_cool():
-        return "Be cool, be coooool! You're almost a FSND grad!"
+    @app.route('/campsites', methods=['POST'])
+    def add_campsite():
+        return "Not implemented!"
+
+
+    @app.route('/campsites', methods=['PATCH'])
+    def update_campsite():
+        return "Not implemented!"
+
+
+    @app.route('/campsites/<int:campsite_id>', methods=['DELETE'])
+    def delete_campsite(campsite_id):
+        return "Not implemented!"
+
+
+    # Error Handling
+    '''
+    422: Unprocessable entity.
+    '''
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+
+    '''
+    404 Error: Resource cannot be found.
+    '''
+
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "not found"
+        }), 404
+
+
+    '''
+    401 Error: User lacks valid authentication credentials for the desired resource
+    '''
+
+
+    @app.errorhandler(401)
+    def unauthorized(error):
+    return jsonify({
+            "success": False,
+            "error": 401,
+            "message": "unauthorized"
+        }), 401
+
+
+    '''
+    403 Error: Access to resource is forbidden.
+    '''
+
+
+    @app.errorhandler(403)
+    def forbidden(error):
+    return jsonify({
+            "success": False,
+            "error": 403,
+            "message": "forbidden"
+        }), 403
+
+
+    '''
+    500 Error: Arises in other cases.
+    '''
+
+
+    @app.errorhandler(500)
+    def unprocessable(error):
+    return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "internal server error"
+        }), 500
 
     return app
 
