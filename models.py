8fd9b4b12c8ea3581a07a8c29272cd81335ea2dd
nikarichardson <nikarichardson@yahoo.com>
2020-02-16 16:50:36 -0800

    update models

--- models.py
+++ models.py
@@ -5,8 +5,7 @@ import os
 import json
 
 #database_path = os.environ.get('DATABASE_URL', None)
-database_path = os.environ.get('DATABASE_URL', None) #postgres://kwwzwozzasiqiw:8e70aed4e726e6e2d96cdf380525fa2884689070fdcda2daf3b0c389d879433f@ec2-18-213-176-229.compute-1.amazonaws.com:5432/df2ce01ne4r1gj
-# os.environ['DATABASE_URL']
+database_path = postgres://kwwzwozzasiqiw:8e70aed4e726e6e2d96cdf380525fa2884689070fdcda2daf3b0c389d879433f@ec2-18-213-176-229.compute-1.amazonaws.com:5432/df2ce01ne4r1gj
 db = SQLAlchemy()
 
 '''
