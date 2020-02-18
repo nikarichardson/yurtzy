4e5913803f4c9f1bfa713fda98b974b41e9623b6
nikarichardson <nikarichardson@yahoo.com>
2020-02-17 16:48:48 -0800

    update app.py

--- app.py
+++ app.py
@@ -56,7 +56,8 @@ def create_app(test_config=None):
     """
     @app.route('/campsites/<int:campsite_id>', methods=['GET'])
     def get_campsites_by_id(campsite_id):
-
+        return str(campsite_id)
+        
         try:
             campsite = Campsite.query.filter(Campsite.id == campsite).all() 
 
