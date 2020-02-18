714111cdb46e499d2de20801648986227e141f57
nikarichardson <nikarichardson@yahoo.com>
2020-02-17 18:35:31 -0800

    update

--- app.py
+++ app.py
@@ -9,233 +9,236 @@ from models import setup_db, Campsite
 
 def create_app(test_config=None):
 
-    app = Flask(__name__)
-    setup_db(app)
-    CORS(app)
+	app = Flask(__name__)
+	setup_db(app)
+	CORS(app)
 
-    @app.route('/')
-    def hello_greeting():
-        """
-        excited = os.environ['EXCITED']
-        greeting = "Hello" 
-        #person = Person(name="Maria", catchphrase="hello!")
-        #person.insert()
-        if excited == 'true': 
-            greeting = greeting + "!!!!!"
-            return greeting 
-        """
-        return "Welcome to the Yurtzy backend site! 🏕️ "   
-        
+	@app.route('/')
+	def hello_greeting():
+		"""
+		excited = os.environ['EXCITED']
+		greeting = "Hello" 
+		#person = Person(name="Maria", catchphrase="hello!")
+		#person.insert()
+		if excited == 'true': 
+			greeting = greeting + "!!!!!"
+			return greeting 
+		"""
+		return "Welcome to the Yurtzy backend site! 🏕️ "   
+		
 
-    """
-    Gets all campsites organized under listings of nearest urban centers.
-    """ 
-    @app.route('/campsites', methods=['GET'])
-    def get_campsites():
-        try:
-            selection = Campsite.query.order_by(Campsite.id).all()
+	"""
+	Gets all campsites organized under listings of nearest urban centers.
+	""" 
+	@app.route('/campsites', methods=['GET'])
+	def get_campsites():
+		try:
+			selection = Campsite.query.order_by(Campsite.id).all()
 
-            if len(selection) == 0:
-                abort(404)
+			if len(selection) == 0:
+				abort(404)
 
-            campsites = []
-            for campsite in selection:
-                campsites.append(campsite.format())
+			campsites = []
+			for campsite in selection:
+				campsites.append(campsite.format())
 
-            return jsonify({
-                'success': True,
-                'campsites': campsites
-            })
+			return jsonify({
+				'success': True,
+				'campsites': campsites
+			})
 
-        except:
-            abort(422)
-
-
-    """
-    Retrieves the campsite with the given campsite id.
-    """
-    @app.route('/campsites/<int:campsite_id>', methods=['GET'])
-    def get_campsites_by_id(campsite_id):
-        try:
-            selection = Campsite.query.get(campsite_id)
-        
-            if selection is not None:
-                return jsonify({
-                    'success': True,
-                'campsite': selection.format()
-                })
-            else:
-                abort(404)
-
-        except:
-            abort(422)
-    
-        
+		except:
+			abort(422)
 
 
-    """
-    """
-    @app.route('/campsites', methods=['POST'])
-    def add_campsite():
-        body = request.get_json()
+	"""
+	Retrieves the campsite with the given campsite id.
+	"""
+	@app.route('/campsites/<int:campsite_id>', methods=['GET'])
+	def get_campsites_by_id(campsite_id):
+		try:
+			selection = Campsite.query.get(campsite_id)
+		
+			if selection is not None:
+				return jsonify({
+					'success': True,
+				'campsite': selection.format()
+				})
+			else:
+				abort(404)
 
-        # if no form data
-        if body is None:
-            abort(404)
-
-        # retrieve form data
-        name=body.get('name') 
-        address = body.get('address') 
-        distance_from_city = body.get('distance_from_city') 
-        closest_city = body.get('closest_city') 
-        image = body.get('image')  
-        website = body.get('website') 
-        description = body.get('description') 
-        costs = body.get('costs') 
-        yurts_and_cabins = body.get('yurts_and_cabins')  
-        bathrooms = body.get('bathrooms') 
-        parking = body.get('parking')  
-        ada_access = body.get('ada_access') 
-        campfires = body.get('campfires') 
-        showers = body.get('showers') 
-        wifi = body.get('wifi')  
-        trash_bins = body.get('trash_bins') 
-        picnic_area = body.get('picnic_area') 
-        pets_allowed = body.get('pets_allowed') 
-        potable_water = body.get('potable_water') 
-        rv_parks = body.get('rv_parks') 
-        hiking = body.get('hiking') 
-        camping = body.get('camping') 
-        biking = body.get('biking') 
-        kayaking = body.get('kayaking') 
-        swimming = body.get('swimming')  
-        cooking_grills = body.get('cooking_grills')  
-        hunting = body.get('hunting') 
-
-        try:
-            campsite = Campsite(name=name,address=address,distance_from_city=distance_from_city,
-            closest_city=closest_city,image=image,website=website,description=description,
-            costs=costs,yurts_and_cabins=yurts_and_cabins,bathrooms=bathrooms,
-            parking=parking,ada_access=ada_access,campfires=campfires,showers=showers,
-            wifi=wifi,trash_bins=trash_bins,picnic_area=picnic_area,pets_allowed=pets_allowed,
-            potable_water=potable_water,rv_parks=rv_parks,hiking=hiking,camping=camping,
-            biking=biking,kayaking=kayaking,swimming=swimming,cooking_grills=cooking_grills,
-            hunting=hunting)
-            campsite.insert()
-
-            campsites = []
-            for campsite in selection:
-                campsites.append(campsite.format())
-
-            return jsonify({
-                'success': True,
-                'campsites': campsites
-            })
-
-        #except:
-        #    abort(422)
+		except:
+			abort(422)
+	
+		
 
 
-    """
-    Allows users to edit the given campsite.
-    """
-    @app.route('/campsites/<int:campsite_id>', methods=['PATCH'])
-    def update_campsite(campsite_id):
-        return "Not implemented!"
+	"""
+	Adds a new campsite to the database. 
+	"""
+	@app.route('/campsites', methods=['POST'])
+	def add_campsite():
+		body = request.get_json()
+
+		# if no form data
+		if body is None:
+			abort(404)
+
+		# retrieve form data
+		name= body.get('name') 
+		address = body.get('address') 
+		distance_from_city = body.get('distance_from_city') 
+		closest_city = body.get('closest_city') 
+		image = body.get('image')  
+		website = body.get('website') 
+		description = body.get('description') 
+		costs = body.get('costs') 
+		yurts_and_cabins = body.get('yurts_and_cabins')  
+		bathrooms = body.get('bathrooms') 
+		parking = body.get('parking')  
+		ada_access = body.get('ada_access') 
+		campfires = body.get('campfires') 
+		showers = body.get('showers') 
+		wifi = body.get('wifi')  
+		trash_bins = body.get('trash_bins') 
+		picnic_area = body.get('picnic_area') 
+		pets_allowed = body.get('pets_allowed') 
+		potable_water = body.get('potable_water') 
+		rv_parks = body.get('rv_parks') 
+		hiking = body.get('hiking') 
+		camping = body.get('camping') 
+		biking = body.get('biking') 
+		kayaking = body.get('kayaking') 
+		swimming = body.get('swimming')  
+		cooking_grills = body.get('cooking_grills')  
+		hunting = body.get('hunting') 
+
+		try:
+			"""
+			campsite = Campsite(name=name,address=address,distance_from_city=distance_from_city,
+			closest_city=closest_city,image=image,website=website,description=description,
+			costs=costs,yurts_and_cabins=yurts_and_cabins,bathrooms=bathrooms,
+			parking=parking,ada_access=ada_access,campfires=campfires,showers=showers,
+			wifi=wifi,trash_bins=trash_bins,picnic_area=picnic_area,pets_allowed=pets_allowed,
+			potable_water=potable_water,rv_parks=rv_parks,hiking=hiking,camping=camping,
+			biking=biking,kayaking=kayaking,swimming=swimming,cooking_grills=cooking_grills,
+			hunting=hunting)
+			campsite.insert()
+			"""
+
+			campsites = []
+			for campsite in selection:
+				campsites.append(campsite.format())
+
+			return jsonify({
+				'success': True,
+				'campsites': None#campsites
+			})
+
+		except:
+			abort(422)
 
 
-    """
-    """
-    @app.route('/campsites/<int:campsite_id>', methods=['DELETE'])
-    def delete_campsite(campsite_id):
-        try:
-            selection = Campsite.query.get(campsite_id)
-
-            if selection is None:
-                abort(404)
-
-            selection.delete()
-
-            return jsonify({
-                'success': True,
-                'delete': campsite_id,
-            })
-
-        except:
-            abort(422)
+	"""
+	Allows users to edit the given campsite.
+	"""
+	@app.route('/campsites/<int:campsite_id>', methods=['PATCH'])
+	def update_campsite(campsite_id):
+		return "Not implemented!"
 
 
-    # Error Handling
-    '''
-    422: Unprocessable entity.
-    '''
-    @app.errorhandler(422)
-    def unprocessable(error):
-        return jsonify({
-            "success": False,
-            "error": 422,
-            "message": "unprocessable"
-        }), 422
+	"""
+	"""
+	@app.route('/campsites/<int:campsite_id>', methods=['DELETE'])
+	def delete_campsite(campsite_id):
+		try:
+			selection = Campsite.query.get(campsite_id)
+
+			if selection is None:
+				abort(404)
+
+			selection.delete()
+
+			return jsonify({
+				'success': True,
+				'delete': campsite_id,
+			})
+
+		except:
+			abort(422)
 
 
-    '''
-    404 Error: Resource cannot be found.
-    '''
+	# Error Handling
+	'''
+	422: Unprocessable entity.
+	'''
+	@app.errorhandler(422)
+	def unprocessable(error):
+		return jsonify({
+			"success": False,
+			"error": 422,
+			"message": "unprocessable"
+		}), 422
 
 
-    @app.errorhandler(404)
-    def not_found(error):
-        return jsonify({
-            "success": False,
-            "error": 404,
-            "message": "not found"
-        }), 404
+	'''
+	404 Error: Resource cannot be found.
+	'''
 
 
-    '''
-    401 Error: User lacks valid authentication credentials for the desired resource
-    '''
+	@app.errorhandler(404)
+	def not_found(error):
+		return jsonify({
+			"success": False,
+			"error": 404,
+			"message": "not found"
+		}), 404
 
 
-    @app.errorhandler(401)
-    def unauthorized(error):
-        return jsonify({
-            "success": False,
-            "error": 401,
-            "message": "unauthorized"
-        }), 401
+	'''
+	401 Error: User lacks valid authentication credentials for the desired resource
+	'''
 
 
-    '''
-    403 Error: Access to resource is forbidden.
-    '''
+	@app.errorhandler(401)
+	def unauthorized(error):
+		return jsonify({
+			"success": False,
+			"error": 401,
+			"message": "unauthorized"
+		}), 401
 
 
-    @app.errorhandler(403)
-    def forbidden(error):
-        return jsonify({
-            "success": False,
-            "error": 403,
-            "message": "forbidden"
-        }), 403
+	'''
+	403 Error: Access to resource is forbidden.
+	'''
 
 
-    '''
-    500 Error: Arises in other cases.
-    '''
+	@app.errorhandler(403)
+	def forbidden(error):
+		return jsonify({
+			"success": False,
+			"error": 403,
+			"message": "forbidden"
+		}), 403
 
 
-    @app.errorhandler(500)
-    def unprocessable(error):
-        return jsonify({
-            "success": False,
-            "error": 500,
-            "message": "internal server error"
-        }), 500
+	'''
+	500 Error: Arises in other cases.
+	'''
 
-    return app
+
+	@app.errorhandler(500)
+	def unprocessable(error):
+		return jsonify({
+			"success": False,
+			"error": 500,
+			"message": "internal server error"
+		}), 500
+
+	return app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
